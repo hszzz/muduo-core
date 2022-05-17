d9460405841996b332131c3135880b0b09c6617f
hszzz <hszzz.me@gmail.com>
2022-05-18 00:38:59 +0800

    feat: channel

--- channel.cc
+++ channel.cc
@@ -1,5 +1,49 @@
 #include "channel.h"
 
+#include <sys/epoll.h>
+
+#include "logger.h"
+
 namespace muduo_core {
+
+void Channel::tie(const std::shared_ptr<void>& obj) {
+  tie_ = obj;
+  tied_ = true;
 }
 
+void Channel::update() {}
+
+void Channel::remove() {}
+
+void Channel::handleEvent(Timestamp receiveTime) {
+  if (tied_) {
+    std::shared_ptr<void> guard = tie_.lock();
+    if (guard) {
+      handleEventWithGuard(receiveTime);
+    }
+  } else {
+    handleEventWithGuard(receiveTime);
+  }
+}
+
+void Channel::handleEventWithGuard(Timestamp receiveTime) {
+  LOG_INFO("channel handle events: %d", revents_);
+
+  if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
+    if (closeCallBack_) closeCallBack_();
+  }
+
+  if (revents_ & EPOLLERR) {
+    if (errorCallBack_) errorCallBack_();
+  }
+
+  if (revents_ & (EPOLLIN | EPOLLPRI)) {
+    if (readCallBack_) readCallBack_(receiveTime);
+  }
+
+  if (revents_ & EPOLLOUT) {
+    if (writeCallBack_) writeCallBack_();
+  }
+}
+
+}  // namespace muduo_core
--- channel.h
+++ channel.h
@@ -16,12 +16,61 @@ class Channel : noncopyable {
   using EventCallBack = std::function<void()>;
   using ReadEventCallback = std::function<void(Timestamp)>;
 
-  Channel(EventLoop* loop, int fd);
-  ~Channel();
+  Channel(EventLoop* loop, int fd)
+      : loop_(loop), fd_(fd), events_(0), index_(-1), tied_(false) {}
+  ~Channel() = default;
 
   void handleEvent(Timestamp receiveTime);
 
+  void setReadCallBack(ReadEventCallback cb) { readCallBack_ = std::move(cb); }
+  void setWriteCallBack(EventCallBack cb) { writeCallBack_ = std::move(cb); }
+  void setCloseCallBack(EventCallBack cb) { closeCallBack_ = std::move(cb); }
+  void setErrorCallBack(EventCallBack cb) { errorCallBack_ = std::move(cb); }
+
+  // 防止 channel 在执行任务中途被手动 remove 掉
+  void tie(const std::shared_ptr<void>&);
+
+  int fd() const { return fd_; }
+  int events() const { return events_; }
+  void set_revents(int revts) { revents_ = revts; }
+
+  void enableReading() {
+    events_ |= kReadEvent;
+    update();
+  }
+  void disableReading() {
+    events_ &= ~kReadEvent;
+    update();
+  }
+  void enableWriting() {
+    events_ |= kWriteEvent;
+    update();
+  }
+  void disableWriting() {
+    events_ &= ~kWriteEvent;
+    update();
+  }
+  void disableAll() {
+    events_ = kNoneEvent;
+    update();
+  }
+
+  // 返回fd当前的事件状态
+  bool isNoneEvent() const { return events_ == kNoneEvent; }
+  bool isWriting() const { return events_ & kWriteEvent; }
+  bool isReading() const { return events_ & kReadEvent; }
+
+  int index() { return index_; }
+  void set_index(int idx) { index_ = idx; }
+
+  // one loop per thread
+  EventLoop* ownerLoop() { return loop_; }
+  void remove();
+
  private:
+  void update();
+  void handleEventWithGuard(Timestamp receiveTime);
+
   static const int kNoneEvent;
   static const int kReadEvent;
   static const int kWriteEvent;
