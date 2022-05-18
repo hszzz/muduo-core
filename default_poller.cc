bc4bdd718f573258851441c02f0b0676a164bc76
hszzz <hszzz.me@gmail.com>
2022-05-18 23:35:43 +0800

    feat: add default_poller

--- default_poller.cc
+++ default_poller.cc
@@ -1,15 +1,16 @@
+#include <stdlib.h>
+
+#include "epoll_poller.h"
 #include "poller.h"
-#include "stdlib.h"
 
 namespace muduo_core {
 
 Poller* Poller::newDefaultPoller(EventLoop* loop) {
-  if (::getenv("MUDUO_USE_POLL"))
-  {
-    return nullptr;
+  if (::getenv("MUDUO_USE_POLL")) {
+    // TODO
+    // return nullptr;
   }
-  return nullptr;
+  return new EPollPoller(loop);
 }
 
-}
-
+}  // namespace muduo_core
