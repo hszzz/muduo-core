2f2360d409f109a81e82ed6ee5f038d8c3efe966
hszzz <hszzz.me@gmail.com>
2022-05-20 22:57:19 +0800

    feat: buffer

--- buffer.h
+++ buffer.h
@@ -1,11 +1,85 @@
 #ifndef MUDUO_CORE_BUFFER_H
 #define MUDUO_CORE_BUFFER_H
 
+#include <string>
+#include <vector>
+
 namespace muduo_core {
 
 class Buffer {
+ public:
+  static const size_t kCheapPrepend = 8;
+  static const size_t kInitialSize = 1024;
+
+  explicit Buffer(size_t initialSize = kInitialSize)
+      : buffer_(kCheapPrepend + initialSize),
+        readerIndex_(kCheapPrepend),
+        writerIndex_(kCheapPrepend) {}
+
+  size_t readableBytes() const { return writerIndex_ - readerIndex_; }
+  size_t writableBytes() const { return buffer_.size() - writerIndex_; }
+  size_t prependableBytes() const { return readerIndex_; }
+
+  const char* peek() const { return begin(); }
+
+  void retrieve(size_t len) {
+    if (len < readableBytes()) {
+      readerIndex_ += len;
+    } else {
+      retrieveAll();
+    }
+  }
+
+  void retrieveAll() { readerIndex_ = writerIndex_ = kCheapPrepend; }
+
+  std::string retrieveAllAsString() {
+    return retrieveAsString(readableBytes());
+  }
+
+  std::string retrieveAsString(size_t len) {
+    std::string result(peek(), len);
+    retrieve(len);
+    return result;
+  }
+
+  void append(const char* data, size_t len) {
+    ensureWritableBytes(len);
+    std::copy(data, data + len, beginWrite());
+    writerIndex_ += len;
+  }
+
+  ssize_t readFd(int fd, int* saveErrno);
+
+  void ensureWritableBytes(size_t len) {
+    if (writableBytes() < len) {
+      makeSpace(len);
+    }
+  }
+
+  char* beginWrite() { return begin() + writerIndex_; }
+  const char* beginWrite() const { return begin() + writerIndex_; }
+
+ private:
+  char* begin() { return &*buffer_.begin(); }
+  const char* begin() const { return &*buffer_.begin(); }
+
+  void makeSpace(size_t len) {
+    if ((writableBytes() + prependableBytes()) < (len + kCheapPrepend)) {
+      buffer_.resize(writerIndex_ + len);
+    } else {
+      size_t readable = readableBytes();
+      std::copy(begin() + readerIndex_, begin() + writerIndex_,
+                begin() + kCheapPrepend);
+      readerIndex_ = kCheapPrepend;
+      writerIndex_ = readerIndex_ + readable;
+    }
+  }
+
+  std::vector<char> buffer_;
+  size_t readerIndex_;
+  size_t writerIndex_;
 };
 
-} // namespace muduo_core
+}  // namespace muduo_core
 
 #endif  //  MUDUO_CORE_BUFFER_H
