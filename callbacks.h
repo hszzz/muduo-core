65a47d630302ab81c19ec64879b71d2d5b6a9379
hszzz <hszzz.me@gmail.com>
2022-05-20 17:49:56 +0800

    feat: callbasks

---
+++ callbacks.h
@@ -0,0 +1,22 @@
+#ifndef MUDUO_CORE_CALLBACKS_H
+#define MUDUO_CORE_CALLBACKS_H
+
+#include <functional>
+#include <memory>
+
+namespace muduo_core {
+
+class Buffer;
+class TcpConnection;
+class Timestamp;
+
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
+using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
+using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
+using WriteCompleteCallback = std::function<void(const TcpConnectionPtr&)>;
+using MessageCallback = std::function<void(TcpConnectionPtr&, Buffer*, Timestamp)>;
+
+} // namespace muduo_core
+
+#endif  //  MUDUO_CORE_CALLBACKS_H
