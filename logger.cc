6bc762b9c8dd36a24d6819b25ca927f5401d5178
hszzz <hszzz.me@gmail.com>
2022-05-21 23:07:37 +0800

    feat: refactor logger

--- logger.cc
+++ logger.cc
@@ -1,36 +1,75 @@
 #include "logger.h"
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 namespace muduo_core {
+
+const char* LogLevel::toString(LogLevel::Level level) {
+  switch (level) {
+#define XX(name)       \
+  case LogLevel::name: \
+    return #name;      \
+    break;
+
+    XX(DEBUG);
+    XX(INFO);
+    XX(WARN);
+    XX(ERROR);
+    XX(FATAL);
+#undef XX
+    default:
+      return "UNKNOW";
+  }
+  return "UNKNOW";
+}
+
+LogLevel::Level LogLevel::fromString(const std::string& str) {
+#define XX(level, v)        \
+  if (str == #v) {          \
+    return LogLevel::level; \
+  }
+
+  XX(DEBUG, debug);
+  XX(INFO, info);
+  XX(WARN, warn);
+  XX(ERROR, error);
+  XX(FATAL, fatal);
+
+  XX(DEBUG, DEBUG);
+  XX(INFO, INFO);
+  XX(WARN, WARN);
+  XX(ERROR, ERROR);
+  XX(FATAL, FATAL);
+
+  return LogLevel::UNKNOWN;
+#undef XX
+}
+
+std::string LogEvent::toString() const {
+  char buf[1024] = {0};
+  ::snprintf(buf, sizeof(buf), " %s %s:%ld:%s -> %s",
+             timestamp_.toString().c_str(), file_, line_, func_, message_);
+  return buf;
+}
+
+void LogConsoleAppender::append(LogLevel::Level level, const LogEvent& event) {
+  std::cout << LogLevel::toString(level) << event.toString() << std::endl;
+}
 
 Logger* Logger::instance() {
   static Logger logger;
   return &logger;
 }
 
-void Logger::setLoggerLevel(LogLevel level) { level_ = level; }
+void Logger::setLevel(LogLevel::Level level) { level_ = level; }
 
-void Logger::log(const std::string& msg) {
-  switch (level_) {
-    case kINFO:
-      std::cout << "INFO";
-      break;
-    case kERROR:
-      std::cout << "ERROR";
-      break;
-    case kFATAL:
-      std::cout << "FATAL";
-      break;
-    case kDEBUG:
-      std::cout << "DEBUG";
-      break;
-    default:
-      break;
+void Logger::log(LogLevel::Level level, const LogEvent& event) {
+  if (level > level_) {
+    for (const auto& appender : appenders_) {
+      appender->append(level, event);
+    }
   }
-
-  std::cout << " " << "placeholder" << ":" << msg << std::endl;
 }
 
 }  // namespace muduo_core
--- logger.h
+++ logger.h
@@ -1,42 +1,120 @@
 #ifndef MUDUO_CORE_LOGGER_H
 #define MUDUO_CORE_LOGGER_H
 
+#include <algorithm>
+#include <list>
+#include <memory>
 #include <string>
-#include <stdio.h>
 
 #include "noncopyable.h"
+#include "timestamp.h"
 
-#define LOG_FORMAT(level, format, ...)            \
-  do {                                            \
-    auto logger = muduo_core::Logger::instance(); \
-    logger->setLoggerLevel(level);                \
-    char buf[1024] = {0};                         \
-    ::snprintf(buf, 1024, format, ##__VA_ARGS__); \
-    logger->log(buf);                             \
+#define LOG_FORMAT(level, format, ...)                                    \
+  do {                                                                    \
+    auto logger = muduo_core::Logger::instance();                         \
+    char buf[1024] = {0};                                                 \
+    ::snprintf(buf, 1024, format, ##__VA_ARGS__);                         \
+    logger->log(level,                                                    \
+                muduo_core::LogEvent(__FILE__, __LINE__, __FUNCTION__, buf)); \
   } while (0)
 
-#define LOG_INFO(format, ...) LOG_FORMAT(muduo_core::kINFO, format, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) LOG_FORMAT(muduo_core::kERROR, format, ##__VA_ARGS__)
-#define LOG_FATAL(format, ...) LOG_FORMAT(muduo_core::kFATAL, format, ##__VA_ARGS__)
-#define LOG_DEBUG(format, ...) LOG_FORMAT(muduo_core::kDEBUG, format, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) \
+  LOG_FORMAT(muduo_core::LogLevel::Level::INFO, format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) \
+  LOG_FORMAT(muduo_core::LogLevel::Level::ERROR, format, ##__VA_ARGS__)
+#define LOG_FATAL(format, ...) \
+  LOG_FORMAT(muduo_core::LogLevel::Level::FATAL, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) \
+  LOG_FORMAT(muduo_core::LogLevel::Level::DEBUG, format, ##__VA_ARGS__)
 
 namespace muduo_core {
-enum LogLevel {
-  kINFO = 0,
-  kERROR,
-  kFATAL,
-  kDEBUG,
+
+struct LogLevel {
+  enum Level {
+    UNKNOWN = 0,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+  };
+
+  static const char* toString(LogLevel::Level level);
+  static LogLevel::Level fromString(const std::string& level);
+};
+
+class LogEvent {
+ public:
+  LogEvent(const char* file, const size_t line, const char* func,
+           const char* message)
+      : timestamp_(Timestamp::now()),
+        file_(file),
+        line_(line),
+        func_(func),
+        message_(message) {}
+
+  std::string toString() const;
+
+ private:
+  Timestamp timestamp_;
+  const char* file_;
+  const size_t line_;
+  const char* func_;
+  const char* message_;
+};
+
+class LogAppender : noncopyable {
+ public:
+  virtual void append(LogLevel::Level, const LogEvent&) = 0;
+};
+
+class LogConsoleAppender : public LogAppender {
+ public:
+  virtual void append(LogLevel::Level, const LogEvent&) override;
 };
 
 class Logger : noncopyable {
  public:
+  using LogAppenderPtr = std::shared_ptr<LogAppender>;
+
   static Logger* instance();
-  void setLoggerLevel(LogLevel level);
-  void log(const std::string& msg);
+
+  void addAppender(LogAppenderPtr appender) { appenders_.push_back(appender); }
+
+  void removeAppender(LogAppenderPtr appender) {
+    for (auto it = appenders_.begin(); it != appenders_.end(); ++it) {
+      if (*it == appender) {
+        appenders_.erase(it);
+        break;
+      }
+    }
+  }
+
+  void enableStdout(bool on) {
+    if (on) {
+      addAppender(stdout_);
+    } else {
+      removeAppender(stdout_);
+    }
+  }
+
+  void setLevel(LogLevel::Level level);
+
+  void log(LogLevel::Level level, const LogEvent& event);
+  void debug(const LogEvent& event) { log(LogLevel::Level::DEBUG, event); }
+  void info(const LogEvent& event) { log(LogLevel::Level::INFO, event); }
+  void warn(const LogEvent& event) { log(LogLevel::Level::WARN, event); }
+  void error(const LogEvent& event) { log(LogLevel::Level::ERROR, event); }
+  void fatal(const LogEvent& event) { log(LogLevel::Level::FATAL, event); }
 
  private:
-  LogLevel level_;
-  Logger() {}
+  Logger() : level_(LogLevel::Level::UNKNOWN), stdout_(new LogConsoleAppender) {
+    enableStdout(true);
+  }
+
+  LogLevel::Level level_;
+  LogAppenderPtr stdout_;
+  std::list<LogAppenderPtr> appenders_;
 };
 
 }  // namespace muduo_core
