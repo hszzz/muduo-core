ae8762904044a14af869c87e60a68e87195c004d
hszzz <hszzz.me@gmail.com>
2022-05-29 22:37:09 +0800

    test: test async logger

--- tests/logger_test.cc
+++ tests/logger_test.cc
@@ -1,9 +1,22 @@
 #include "../logger.h"
 
-int main(){
-  LOG_INFO("%s %s", "123", "123");
-  LOG_DEBUG("123");
-  LOG_FATAL("123");
-  LOG_ERROR("123");
+#include <thread>
+
+void producer() {
+  while (1) {
+    LOG_INFO("messgae: %s", "test");
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
 }
 
+int main() {
+  LOG_INFO("%s %s", "123", "123");
+  LOG_DEBUG("123");
+  // LOG_FATAL("123");
+  LOG_ERROR("123");
+
+  std::thread t1(producer);
+  std::thread t2(producer);
+  t1.join();
+  t2.join();
+}
